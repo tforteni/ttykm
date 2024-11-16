de7cea57c5b18505e163260cbde3e9f9c7497c42
isaimel <isaimel001@gmail.com>
2024-11-16 13:28:17 -0500

    Create piece.py

---
+++ piece.py
@@ -0,0 +1,12 @@
+class Piece():
+
+    def __init__(self, symbol):
+        if type(symbol) == str and len(symbol) == 1:
+            self._symbol = symbol
+        else:
+            self._symbol = "E" # "E" for Error
+            
+            
+        
+    def __repr__(self):
+        return "" + self._symbol
