643be15ec22eeda77b8885634364d18c267547ef
isaimel <isaimel001@gmail.com>
2024-11-16 23:18:01 -0500

    Update enumerate possible moves

--- game.py
+++ game.py
@@ -78,12 +78,152 @@ class Game:
             game.set_move_strategy(Move())
         self._strategy.move(self, piece, row, column, board, player, direction, leave_copy)
     
-    def enumerate_possible_moves(self, piece):
-        possible_moves =  []
-        moves_dictionary =  ["n", "s", "e", "w", "b", "f"]
+    def enumerate_possible_moves(self, column, row, board, player):
+        round1 = self.enumerate_possible_moves_helper(column, row, board, player)
+        round1_possible_moves = round1[0]
+        round1_locations = round1[1]
+        final_list = []
         
-        row= piece.row
-        column = piece.column
+        # goes through the possible moves from a "first round", uses the locations after the movement
+        # is applied, and then adds all variations to the first round in a list
+        for x in range(0, len(round1_possible_moves)):
+            round2 = self.enumerate_possible_moves_helper(round1_locations[x]["column"],
+                                                          round1_locations[x]["row"],
+                                                          round1_locations[x]["board"],
+                                                          player)
+            round2_possible_moves = round2[0]            
+
+            for y in range(0, len(round2_possible_moves)):
+                final_list.append((round1_possible_moves[x], round2_possible_moves[y]))
+        
+        if len(final_list) == 0:
+            for x in range(0, len(round1_possible_moves)):
+                final_list.append((round1_possible_moves[x], None))
+                
+        return final_list
+
+    
+    #Assumes the piece is a piece is a valid Piece object
+    def enumerate_possible_moves_helper(self, column, row, board, player):
+        class AbstractCommand:
+            def execute(self):
+                raise NotImplemented()
+
+        class north(AbstractCommand):
+            def __init__(self, location):
+                self.location = location
+                self.symbol = "n"
+            def execute(self):
+                self.location["column"] = self.location["column"] - 1 
+                
+        class east(AbstractCommand):
+            def __init__(self, location):
+                self.location = location
+                self.symbol = "e"
+            def execute(self):
+                self.location["row"] = self.location["row"] + 1 
+                
+        class south(AbstractCommand):
+            def __init__(self, location):
+                self.location = location
+                self.symbol = "s"
+            def execute(self):
+                self.location["column"] = self.location["column"] + 1 
+        
+        class west(AbstractCommand):
+            def __init__(self, location):
+                self.location = location
+                self.symbol = "w"
+            def execute(self):
+                self.location["row"] = self.location["row"] - 1 
+                
+        class forward(AbstractCommand):
+            def __init__(self, location):
+                self.location = location
+                self.symbol = "f"
+            def execute(self):
+                self.location["board"] = self.location["board"] + 1 
+
+        class backward(AbstractCommand):
+            def __init__(self, location):
+                self.location = location
+                self.symbol = "b"
+            def execute(self):
+                self.location["board"] = self.location["board"] - 1 
+        
+        
+        # location =  {"column" : piece.column, "row" : piece.row, "board" : piece.location}
+        moves_round_one = [north({"column" : column, "row" : row, "board" : board}), 
+                           east({"column" : column, "row" : row, "board" : board}), 
+                           south({"column" : column, "row" : row, "board" : board}), 
+                           west({"column" : column, "row" : row, "board" : board}), 
+                           forward({"column" : column, "row" : row, "board" : board}), 
+                           backward({"column" : column, "row" : row, "board" : board})]
+        valid_moves = []
+        new_locations = []
+
+        
+        for x in moves_round_one:
+            x.execute()
+            
+            #immediately skips to the next iteration if piece is out of bounds
+            if not (0 <= x.location["column"] < self.all_boards[0]._columns
+                    and 0 <= x.location["row"] < self.all_boards[0]._rows
+                    and 0 <= x.location["board"] < len(self.all_boards)):
+                continue
+            #checks to see if the location where the piece moved is occupied by not none
+            if self.all_boards[x.location["board"]].occupied(x.location["row"], x.location["column"]) != None:
+                #checks to see if the player does not own this piece, if so, continue to next iteration
+                if player.owns_piece(self.all_boards[x.location["board"]].occupied(x.location["row"], x.location["column"]).symbol) == True:
+                    continue
+                # if the piece has jumped boards and that space is not None, then it is not a viable move
+                if board != x.location["board"]:
+                    continue
+            valid_moves.append(x.symbol)
+            new_locations.append(x.location)
+
+        return [valid_moves, new_locations]
+
+from player import Player    
+if __name__ == "__main__":
+    player1 = Player("white")
+    
+    player1._all_pieces[5].row = 1
+    player1._all_pieces[5].column = 0
+    player1._all_pieces[5].location = 2
+    
+    player1._all_pieces[4].row = 0
+    player1._all_pieces[4].column = 1
+    player1._all_pieces[4].location = 2
+    
+    player1._all_pieces[6].row = 0
+    player1._all_pieces[6].column = 0
+    player1._all_pieces[6].location = 2
+    player2 = Player("brown")
+    game = Game(player1, player2)
+    test = game.enumerate_possible_moves(player1._all_pieces[6].column,
+                                        player1._all_pieces[6].row,
+                                        player1._all_pieces[6].location,
+                                        player1)
+    
+    for x in test:
+        print(x, '\n')
+    
+                
+            
+            
+            
+        
+        
+
+
+        
+        
+        # #location is [row][column]
+        # information = (piece.row, )        
+        #         moves_dictionary =  ["n", "s", "e", "w", "b", "f"]
+        
+
         
                 
         
--- piece.py
+++ piece.py
@@ -1,10 +1,10 @@
 class Piece():
 
-    def __init__(self, symbol, inplay = False, alive = True, board = -1, row = -1, column = -1):
+    def __init__(self, symbol, inplay = False, alive = True, location = -1, row = -1, column = -1):
         self.symbol = symbol
         self.in_play = inplay
         self.alive = alive
-        self.location = board
+        self.location = location
         self.row = row
         self.column = column
         
