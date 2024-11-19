d1455f7e70bdf613fb920d5f4b465fd8854efde7
isaimel <isaimel001@gmail.com>
2024-11-18 23:21:54 -0500

    Update game.py

--- game.py
+++ game.py
@@ -223,7 +223,7 @@ class Game:
                     continue                
                 #if player does own the piece and if the symbol in the moves square does not match the symbol of the original piece
                 if player.owns_piece(chosen_piece.symbol) != None and chosen_piece.symbol != symbol:
-                    if not any(x.row == row and x.column == column and x.location == board for x in other._all_pieces):
+                    if not any(y.row == row and y.column == column and y.location == board for y in other._all_pieces) and x.symbol != prev_move:
                         continue
                 
             # checks to see if the player does not have any pieces in the case of a movement to the past
